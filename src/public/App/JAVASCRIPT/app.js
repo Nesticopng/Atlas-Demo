1b4e66306e460b20dd8668f10e20963dbee58b9d
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 10:21:59 -0400

    Update app.js

--- src/public/App/JAVASCRIPT/app.js
+++ src/public/App/JAVASCRIPT/app.js
@@ -39,7 +39,19 @@ const today = new Date()
 const year = today.getFullYear()
 const month = String(today.getMonth() + 1).padStart(2, '0')
 const day = String(today.getDate()).padStart(2, '0')
+    
+const yesterday = new Date(today)
+      yesterday.setDate(today.getDate() - 1)
 
+const year_y = yesterday.getFullYear()
+const month_y = String(yesterday.getMonth() + 1).padStart(2, '0')
+const day_y = String(yesterday.getDate()).padStart(2, '0')
+
+fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?base=USD&start_date=${year_y}-${month_y}-${day_y}&end_date=${year}-${month}-${day}`, requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result)
+    .catch(error => res.json(error))
+}
 function ToggleTheme(){
     document.body.classList.toggle('dark')
     
