df400b3a51f4014fcd17779acdc67d08957336a9
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 10:14:32 -0400

    Update index.controllers.js

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -47,7 +47,7 @@ indexCtrl.API_INFLATION = (req, res) => {
     fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?base=USD&start_date=${year_y}-${month_y}-${day_y}&end_date=${year}-${month}-${day}`, requestOptions)
         .then(response => response.json())
         .then(result => res.json(result.rates.VES))
-        .catch(error => res.json(error)
+        .catch(error => res.json(error))
 }
 
 
