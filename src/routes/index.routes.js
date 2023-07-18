1cada985917f2660ea49a8cde37d79b4ed41a988
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 11:26:46 -0400

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 const { isAuthenticated } = require('../helpers/auth')
-const { renderIndex, renderLogin, renderApp, APILayer, renderTransactions, API_INFLATION, renderRecharge } = require('../controllers/index.controllers')
+const { renderIndex, renderLogin, renderApp, APILayer, renderTransactions, renderRecharge } = require('../controllers/index.controllers')
 
 router.get('/' ,renderIndex)
 
@@ -14,8 +14,5 @@ router.get('/App/API-Price-Data', isAuthenticated, APILayer)
 router.get('/App/Transactions', isAuthenticated, renderTransactions)
 
 router.get('/App/Recharge', isAuthenticated, renderRecharge)
-
-router.get('/App/API-Price', isAuthenticated, API_INFLATION)
-
 
 module.exports = router
