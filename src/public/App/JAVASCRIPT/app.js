10ecf0f01b11157f958774172e0ab438af82a1e8
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 10:53:05 -0400

    Update app.js

--- src/public/App/JAVASCRIPT/app.js
+++ src/public/App/JAVASCRIPT/app.js
@@ -39,29 +39,6 @@ const today = new Date()
 const year = today.getFullYear()
 const month = String(today.getMonth() + 1).padStart(2, '0')
 const day = String(today.getDate()).padStart(2, '0')
-    
-const yesterday = new Date(today)
-      yesterday.setDate(today.getDate() - 1)
-
-const year_y = yesterday.getFullYear()
-const month_y = String(yesterday.getMonth() + 1).padStart(2, '0')
-const day_y = String(yesterday.getDate()).padStart(2, '0')
-
-
-const API_KEY = "ZkFb0ZocGPNkjKzyZdIAmmbfTYs51i5G"
-
-const myHeaders = { apikey: API_KEY }
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-}
-
-fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?base=USD&start_date=${year_y}-${month_y}-${day_y}&end_date=${year}-${month}-${day}`, requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
 
 function ToggleTheme(){
     document.body.classList.toggle('dark')
@@ -172,6 +149,10 @@ function showTime(){
 }
 
 showTime()
+    fetch('/App/API-Price-Data')
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('ERROR', error))
 
 Promise.all([fetchData, fetchPriceData , fetchHistorial ])
     .then(responses => {
