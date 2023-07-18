fdb8c0de1eca8bb3be014948591af2c02bc7cdd8
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 11:04:45 -0400

    Update index.controllers.js

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -2,15 +2,6 @@ require('dotenv').config()
 
 const indexCtrl = {}
 
-const API_KEY = process.env.API_KEY
-
-const myHeaders = { apikey: API_KEY }
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-}
 
 indexCtrl.renderIndex = (req, res) =>{
     res.render("./home") 
@@ -44,6 +35,16 @@ indexCtrl.API_INFLATION = (req, res) => {
     const year_y = yesterday.getFullYear()
     const month_y = String(yesterday.getMonth() + 1).padStart(2, '0')
     const day_y = String(yesterday.getDate()).padStart(2, '0') 
+    
+    const API_KEY="ZkFb0ZocGPNkjKzyZdIAmmbfTYs51i5G"
+
+    const myHeaders = { apikey: API_KEY }
+    
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    }
     
     fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?base=USD&start_date=${year_y}-${month_y}-${day_y}&end_date=${year}-${month}-${day}`, requestOptions)
         .then(response => response.json())
