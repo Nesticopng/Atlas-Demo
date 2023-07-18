43285e519218bb3c855da7af3a670e6d25ba5f2f
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-18 10:09:10 -0400

    Update index.controllers.js

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -43,7 +43,11 @@ indexCtrl.API_INFLATION = (req, res) => {
     const year_y = yesterday.getFullYear()
     const month_y = String(yesterday.getMonth() + 1).padStart(2, '0')
     const day_y = String(yesterday.getDate()).padStart(2, '0')
-    res.json(yesterday)
+    
+    fetch(`https://api.apilayer.com/exchangerates_data/fluctuation?base=USD&start_date=${year_y}-${month_y}-${day_y}&end_date=${year}-${month}-${day}`, requestOptions)
+        .then(response => response.json())
+        .then(result => res.json(result.rates.VES))
+        .catch(error => console.log('ERROR', error)
 }
 
 
