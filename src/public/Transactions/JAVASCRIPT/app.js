1530c4b3eac8bd80bbd0cf36c2243ddea4c23a67
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-07-20 14:16:03 -0400

    Update app.js

--- src/public/Transactions/JAVASCRIPT/app.js
+++ src/public/Transactions/JAVASCRIPT/app.js
@@ -320,44 +320,6 @@ function ReadedQR(result){
     }
 }
 
-function val(event){
-    event.preventDefault()
-
-    var emailInput = document.getElementById("email").value
-    var amountInput = document.getElementById("amount").value
-
-    if(validarCorreo(emailInput) && emailInput != email && amountInput < balance){
-        localStorage.setItem('Alert', 'La transaccion ha sido completada')
-            var datos = {
-            name: nameInput,
-            email: correoInput,
-            Newnumero: numeroInput,
-            Newcdi: cdiInput,
-            password: passwordInput,
-            confirm_password: confirmPasswordInput
-        }
-
-        fetch("/SingUpReg", {
-            method: 'POST',
-            body: JSON.stringify(datos),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        event.target.submit()
-        
-    }else if(emailInput === email){
-        const err = `No puedes transferirte a ti mismo`
-        ErrorMSG(err)
-    }else if(amountInput > balance){
-        const err = `El monto es mayor a su saldo <br> por favor recargue` 
-        ErrorMSG(err)
-    }else if(!validarCorreo(emailInput)){
-        const err = 'El correo no es válido'
-        ErrorMSG(err)
-    }
-}
-
 function PayQR(dataReObj){
     const popupOverlay = document.createElement('div')
     popupOverlay.className = 'popup-overlay'
